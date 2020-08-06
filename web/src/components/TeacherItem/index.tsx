9f63f7d6a06ea0d3f018ee9b2cedb1ce06d8ed64
Ewerson <ewersonv@gmail.com>
2020-08-05 21:19:01 -0300

    Finish third day

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -2,34 +2,56 @@ import React from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
+import api from '../../services/api';
+
 import './styles.css';
 
-const TeacherItem: React.FC = () => {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id
+    });
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://avatars0.githubusercontent.com/u/22565566?s=460&u=3fd9c670ab6847482bc3ea79c91786c02f435e46&v=4" alt="Ewerson Vieira"/>
+        <img src={teacher.avatar} alt={teacher.name}/>
         <div>
-          <strong>Ewerson Vieira</strong>
-          <span>Defesa Contra As Artes Das Trevas</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Entusiasta dos melhores feitiços defensivos.
-        <br /><br />
-        Apaixonado por desarmar os oponentes e deixá-los com as opções de fugir ou se entregar.
-      </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$: 200,00</strong>
+          <strong>R$: {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a
+          target="_blank"
+          onClick={createNewConnection}
+          href={`https://wa.me/${teacher.whatsapp}`}
+        >
           <img src={whatsappIcon} alt="WhatsApp"/>
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,37 +1,91 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 
 import PageHeader from '../../components/PageHeader';
-import TeacherItem from '../../components/TeacherItem';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
+
+import api from '../../services/api';
 
 import './styles.css';
 
 const TeacherList: React.FC = () => {
+  const [teachers, setTeachers] = useState([]);
+
+  const [subject, setSubject] = useState('');
+  const [week_day, setWeekday] = useState('');
+  const [time, setTime] = useState('');
+
+  async function searchTeachers(e: FormEvent) {
+    e.preventDefault();
+
+    const response = await api.get('classes', {
+      params: {
+        subject,
+        week_day,
+        time
+      }
+    });
+
+    setTeachers(response.data);
+  }
+
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffys disponíveis.">
-        <form id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">Matéria</label>
-            <input type="text" id="subject" />
-          </div>
+        <form id="search-teachers" onSubmit={searchTeachers}>
+          <Select
+            name="subject"
+            label="Matéria"
+            value={subject}
+            onChange={e => { setSubject(e.target.value) }}
+            options={[
+              { value: 'Defesa Contra As Artes Das Trevas', label: 'Defesa Contra As Artes Das Trevas' },
+              { value: 'Herbologia', label: 'Herbologia' },
+              { value: 'Transfiguração', label: 'Transfiguração' },
+              { value: 'Trato Das Criaturas Mágicas', label: 'Trato Das Criaturas Mágicas' },
+              { value: 'Poções', label: 'Poções' },
+              { value: 'Astronomia', label: 'Astronomia' },
+              { value: 'Adivinhação', label: 'Adivinhação' },
+              { value: 'Feitiços', label: 'Feitiços' },
+              { value: 'História Da Magia', label: 'História Da Magia' },
+              { value: 'Estudo Das Runas Antigas', label: 'Estudo Das Runas Antigas' },
+              { value: 'Estudo Dos Trouxas', label: 'Estudo Dos Trouxas' },
+            ]}
+          />
+          <Select
+            name="week_day"
+            label="Dia da semana"
+            value={week_day}
+            onChange={e => { setWeekday(e.target.value) }}
+            options={[
+              { value: '0', label: 'Domingo' },
+              { value: '1', label: 'Segunda-feira' },
+              { value: '2', label: 'Terça-feira' },
+              { value: '3', label: 'Quarta-feira' },
+              { value: '4', label: 'Quinta-feira' },
+              { value: '5', label: 'Sexta-feira' },
+              { value: '6', label: 'Sábado' },
+            ]}
+          />
+          <Input
+            type="time"
+            name="time"
+            label="Hora"
+            value={time}
+            onChange={e => { setTime(e.target.value) }}
+          />
 
-          <div className="input-block">
-            <label htmlFor="week_day">Dia da semana</label>
-            <input type="text" id="week_day" />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="time">Hora</label>
-            <input type="text" id="time" />
-          </div>
+          <button type="submit">
+            Buscar
+          </button>
         </form>
       </PageHeader>
 
       <main>
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
+        {teachers.map((teacher: Teacher) => {
+          return <TeacherItem key={teacher.id} teacher={teacher} />
+        })}
       </main>
     </div>
   );
